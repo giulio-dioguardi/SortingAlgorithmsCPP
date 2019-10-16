0266665955867512ee303f94b1de5e12e14fb549
Giulio Dioguardi <giulio.dioguardi@alten.nl>
2019-10-16 11:51:27 +0200

    Refactor

--- SortingAlgorithms/bubblesortalgorithm.cpp
+++ SortingAlgorithms/bubblesortalgorithm.cpp
@@ -11,11 +11,12 @@ BubbleSortAlgorithm::~BubbleSortAlgorithm()
 //template <typename T> T* SortingAlgorithm::sort(T* inputarray)
 void BubbleSortAlgorithm::sort(std::vector<int>* inputarray)
 {
-    if (inputarray->size() == 1)
+    if (inputarray->size() < 2)
     {
         return;
     }
-    else if (inputarray->size() == 2)
+
+    if (inputarray->size() == 2)
     {
         std::iter_swap(inputarray->begin(), inputarray->begin() + 1);
     }
--- SortingAlgorithmsTest/tst_sorttest.h
+++ SortingAlgorithmsTest/tst_sorttest.h
@@ -21,30 +21,34 @@ public:
     {
         delete(this->algorithm);
     }
+
+    void actAndAssert(std::vector<int>* input, std::vector<int>* output)
+    {
+        algorithm->sort(input);
+        EXPECT_EQ(*input, *output);
+    }
 };
 
 
 TEST_F(BubbleSortAlgorithmTest, emptyVector)
 {
-    std::vector<int> array;
-    algorithm->sort(&array);
-    ASSERT_EQ(array, std::vector<int>());
+    std::vector<int> vectorInput;
+    std::vector<int> vectorExpected = {};
+    actAndAssert(&vectorInput, &vectorExpected);
 }
 
 TEST_F(BubbleSortAlgorithmTest, vectorWithOneEntry)
 {
     std::vector<int> vectorInput = { 1 };
     std::vector<int> vectorExpected = { 1 };
-    algorithm->sort(&vectorInput);
-    ASSERT_EQ(vectorInput, vectorExpected);
+    actAndAssert(&vectorInput, &vectorExpected);
 }
 
 TEST_F(BubbleSortAlgorithmTest, vectorWithTwoEntriesOutOfOrder)
 {
     std::vector<int> vectorInput = { 2, 1 };
     std::vector<int> vectorExpected = { 1 , 2 };
-    algorithm->sort(&vectorInput);
-    ASSERT_EQ(vectorInput, vectorExpected);
+    actAndAssert(&vectorInput, &vectorExpected);
 }
 
 
