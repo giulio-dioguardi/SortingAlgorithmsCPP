10bf35432fb0e4a32fd190264fad97d54f2ad2fe
Giulio Dioguardi <giulio.dioguardi@alten.nl>
2019-10-25 10:15:09 +0200

    Edited main

--- SortingAlgorithms/main.cpp
+++ SortingAlgorithms/main.cpp
@@ -6,12 +6,17 @@
 
 using namespace std;
 
-int main()
+template <typename T>
+T fRand(T fMin, T fMax)
 {
-    SortingAlgorithm* algorithm = new BubbleSortAlgorithm();
-    std::vector<double> vectorInput = {};
-    size_t size = 24444;
-    for (size_t i = 0 ; i < size; i++)
+    T f = static_cast<T>(rand()) / RAND_MAX;
+    return fMin + f * (fMax - fMin);
+}
+
+void speedTest(SortingAlgorithm* algorithm, int size)
+{
+    std::vector<int> vectorInput = {};
+    for (auto i = 0 ; i < size; i++)
     {
         vectorInput.push_back(rand() % size);
     }
@@ -20,5 +25,18 @@ int main()
     clock_t end = clock();
     double elapsed_seconds = double(end - begin) / CLOCKS_PER_SEC;
     cout << "Time to sort " << size << " elements, using " << algorithm->toString() << " : " << elapsed_seconds << endl;
+    delete(algorithm);
+}
+
+int main()
+{
+    speedTest(new QuickSortAlgorithm(), 2000);
+    speedTest(new BubbleSortAlgorithm(), 200);
+    speedTest(new QuickSortAlgorithm(), 20000);
+    speedTest(new BubbleSortAlgorithm(), 2000);
+    speedTest(new QuickSortAlgorithm(), 100000);
+    speedTest(new BubbleSortAlgorithm(), 10000);
+    speedTest(new QuickSortAlgorithm(), 1000000);
+    speedTest(new BubbleSortAlgorithm(), 20000);
     return 0;
 }
